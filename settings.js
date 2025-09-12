8b4c1175107c214108e2ad560b093e08e87676ad
Tamim <gojo82851@gmail.com>
2025-09-12 15:28:37 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "SANJU BOT",
+  botName: "SASUKE BOT",
   botOwner: 'TAMIM', // Your name
-  ownerNumber: '8801719741293', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '8801405706180', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.0.8",
+  version: "1.0.0",
 };
 
 module.exports = settings;
